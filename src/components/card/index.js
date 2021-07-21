c5b23a497f0ba3937e6982b79e6fa8918f5db000
Yashika <yashika.lamba.phe19@itbhu.ac.in>
2021-07-22 00:54:07 +0530

    icons-added

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './style.css'
 
 const Card = (props) => {
@@ -9,13 +11,17 @@ const Card = (props) => {
                 <div class="portfolio-thumb">
                     <img src={props.image} />
                 </div>
-                <div class="portfolio-caption text-center">
-                    <h4>{props.title}</h4>
+                <div class="portfolio-caption text-center" style={{opacity: '.9'}}>
+                    <h3 style={{color: 'white'}}>{props.title}</h3>
                     <div class="work-tag">
-                        <a href={props.github} style={{paddingLeft: '2px'}}>Github</a>
-                        <span>,</span>
-                        <a href={props.web}>Preview</a>
-                        <span>,</span>
+                        <ul style={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
+                        <li style={{justifyContent: 'center', alignContent: 'center', marginRight: '2%', marginLeft: '2%'}}>
+                        <a href={props.github} style={{color: 'white'}}><h2><FontAwesomeIcon icon={faGithub} /></h2></a>
+                        </li>
+                        <li style={{justifyContent: 'center', alignContent: 'center', marginRight: '2%', marginLeft: '2%'}}>
+                        <a href={props.web} style={{color: 'white'}}><h2><FontAwesomeIcon icon={faCode} /></h2></a>
+                        </li>
+                        </ul>
                     </div>
                 </div>
             </div>
